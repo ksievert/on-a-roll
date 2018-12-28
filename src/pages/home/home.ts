5c18c2c08af70f911e79080793f32db641335a1c
Katia Sievert <ksievert@macalester.edu>
2018-12-28 17:29:22 -0600

    initial app with alerts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,8 +6,20 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  rolls = [];
+  temp = [];
   constructor(public navCtrl: NavController) {
+
+  }
+
+  roll(int): void{
+    var result = Math.floor(Math.random() * int) + 1;
+    this.rolls.push[result];
+    alert(result);
+  }
+
+  display(): void{
+    this.temp = this.rolls;
 
   }
 
