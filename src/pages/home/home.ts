7d240c81cff80c4b20c50581643fb1c244056a4b
Katia Sievert <ksievert@macalester.edu>
2018-12-29 18:35:20 -0600

    bold working, diff lines still

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,6 +13,7 @@ export class HomePage {
   }
 
   roll(int): void{
+    document.getElementById("20first").innerHTML = "";
     document.getElementById("20rolls").innerHTML = "";
     var result = Math.floor(Math.random() * int) + 1;
     this.rolls.unshift(result);
@@ -20,7 +21,10 @@ export class HomePage {
       this.rolls.pop();
     }
     //alert(this.rolls);
-    document.getElementById("20rolls").innerHTML += this.rolls;
+    document.getElementById("20first").innerHTML += this.rolls[0];
+    for(var i = 1; i < this.rolls.length; i++) {
+      document.getElementById("20rolls").innerHTML += ", " + this.rolls[i];
+    }
   }
 
   display(): void{
